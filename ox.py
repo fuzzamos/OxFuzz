19c7b7a8e7c80a162ccbfa1298f19e23f59c2fc4
hucvbty'macos <hucvbty@gmail.com>
2018-04-11 16:09:22 +0800

    update

--- ox.py
+++ ox.py
@@ -25,282 +25,14 @@ import sys
 
 from grammar import Grammar
 
-_N_MAIN_LINES = 4
+_N_MAIN_LINES = 10
 _N_EVENTHANDLER_LINES = 500
 
-_N_ADDITIONAL_HTMLVARS = 5
+_N_ADDITIONAL_HTMLVARS = 0
 
-# A map from tag name to corresponding type for HTML tags
-_HTML_TYPES = {
-    'a': 'HTMLAnchorElement',
-    'abbr': 'HTMLUnknownElement',
-    'acronym': 'HTMLUnknownElement',
-    'address': 'HTMLUnknownElement',
-    'applet': 'HTMLUnknownElement',
-    'area': 'HTMLAreaElement',
-    'article': 'HTMLUnknownElement',
-    'aside': 'HTMLUnknownElement',
-    'audio': 'HTMLAudioElement',
-    'b': 'HTMLUnknownElement',
-    'base': 'HTMLBaseElement',
-    'basefont': 'HTMLUnknownElement',
-    'bdi': 'HTMLUnknownElement',
-    'bdo': 'HTMLUnknownElement',
-    'bgsound': 'HTMLUnknownElement',
-    'big': 'HTMLUnknownElement',
-    'blockquote': 'HTMLUnknownElement',
-    'br': 'HTMLBRElement',
-    'button': 'HTMLButtonElement',
-    'canvas': 'HTMLCanvasElement',
-    'caption': 'HTMLTableCaptionElement',
-    'center': 'HTMLUnknownElement',
-    'cite': 'HTMLUnknownElement',
-    'code': 'HTMLUnknownElement',
-    'col': 'HTMLTableColElement',
-    'colgroup': 'HTMLUnknownElement',
-    'command': 'HTMLUnknownElement',
-    'content': 'HTMLContentElement',
-    'data': 'HTMLDataElement',
-    'datalist': 'HTMLDataListElement',
-    'dd': 'HTMLUnknownElement',
-    'del': 'HTMLModElement',
-    'details': 'HTMLDetailsElement',
-    'dfn': 'HTMLUnknownElement',
-    'dialog': 'HTMLDialogElement',
-    'dir': 'HTMLDirectoryElement',
-    'div': 'HTMLDivElement',
-    'dl': 'HTMLDListElement',
-    'dt': 'HTMLUnknownElement',
-    'em': 'HTMLUnknownElement',
-    'embed': 'HTMLEmbedElement',
-    'fieldset': 'HTMLFieldSetElement',
-    'figcaption': 'HTMLUnknownElement',
-    'figure': 'HTMLUnknownElement',
-    'font': 'HTMLFontElement',
-    'footer': 'HTMLUnknownElement',
-    'form': 'HTMLFormElement',
-    'frame': 'HTMLFrameElement',
-    'frameset': 'HTMLFrameSetElement',
-    'h1': 'HTMLHeadingElement',
-    'h2': 'HTMLHeadingElement',
-    'h3': 'HTMLHeadingElement',
-    'h4': 'HTMLHeadingElement',
-    'h5': 'HTMLHeadingElement',
-    'h6': 'HTMLHeadingElement',
-    'header': 'HTMLUnknownElement',
-    'hgroup': 'HTMLUnknownElement',
-    'hr': 'HTMLHRElement',
-    'i': 'HTMLUnknownElement',
-    'iframe': 'HTMLIFrameElement',
-    'image': 'HTMLImageElement',
-    'img': 'HTMLImageElement',
-    'input': 'HTMLInputElement',
-    'ins': 'HTMLModElement',
-    'isindex': 'HTMLUnknownElement',
-    'kbd': 'HTMLUnknownElement',
-    'keygen': 'HTMLKeygenElement',
-    'label': 'HTMLLabelElement',
-    'layer': 'HTMLUnknownElement',
-    'legend': 'HTMLLegendElement',
-    'li': 'HTMLLIElement',
-    'link': 'HTMLLinkElement',
-    'listing': 'HTMLUnknownElement',
-    'main': 'HTMLUnknownElement',
-    'map': 'HTMLMapElement',
-    'mark': 'HTMLUnknownElement',
-    'marquee': 'HTMLMarqueeElement',
-    'menu': 'HTMLMenuElement',
-    'menuitem': 'HTMLMenuItemElement',
-    'meta': 'HTMLMetaElement',
-    'meter': 'HTMLMeterElement',
-    'nav': 'HTMLUnknownElement',
-    'nobr': 'HTMLUnknownElement',
-    'noembed': 'HTMLUnknownElement',
-    'noframes': 'HTMLUnknownElement',
-    'nolayer': 'HTMLUnknownElement',
-    'noscript': 'HTMLUnknownElement',
-    'object': 'HTMLObjectElement',
-    'ol': 'HTMLOListElement',
-    'optgroup': 'HTMLOptGroupElement',
-    'option': 'HTMLOptionElement',
-    'output': 'HTMLOutputElement',
-    'p': 'HTMLParagraphElement',
-    'param': 'HTMLParamElement',
-    'picture': 'HTMLPictureElement',
-    'plaintext': 'HTMLUnknownElement',
-    'pre': 'HTMLPreElement',
-    'progress': 'HTMLProgressElement',
-    'q': 'HTMLQuoteElement',
-    'rp': 'HTMLUnknownElement',
-    'rt': 'HTMLUnknownElement',
-    'ruby': 'HTMLUnknownElement',
-    's': 'HTMLUnknownElement',
-    'samp': 'HTMLUnknownElement',
-    'section': 'HTMLUnknownElement',
-    'select': 'HTMLSelectElement',
-    'shadow': 'HTMLShadowElement',
-    'small': 'HTMLUnknownElement',
-    'source': 'HTMLSourceElement',
-    'span': 'HTMLSpanElement',
-    'strike': 'HTMLUnknownElement',
-    'strong': 'HTMLUnknownElement',
-    'style': 'HTMLStyleElement',
-    'sub': 'HTMLUnknownElement',
-    'summary': 'HTMLUnknownElement',
-    'sup': 'HTMLUnknownElement',
-    'table': 'HTMLTableElement',
-    'tbody': 'HTMLTableSectionElement',
-    'td': 'HTMLUnknownElement',
-    'template': 'HTMLTemplateElement',
-    'textarea': 'HTMLTextAreaElement',
-    'tfoot': 'HTMLTableSectionElement',
-    'th': 'HTMLTableCellElement',
-    'thead': 'HTMLTableSectionElement',
-    'time': 'HTMLTimeElement',
-    'title': 'HTMLTitleElement',
-    'tr': 'HTMLTableRowElement',
-    'track': 'HTMLTrackElement',
-    'tt': 'HTMLUnknownElement',
-    'u': 'HTMLUnknownElement',
-    'ul': 'HTMLUListElement',
-    'var': 'HTMLUnknownElement',
-    'video': 'HTMLVideoElement',
-    'wbr': 'HTMLUnknownElement',
-    'xmp': 'HTMLUnknownElement'
-}
+_INTERNAL_TEMPLATE_PATH = '.\\ox_template.html'
+_INTERNAL_SYNTAX_PATH = '.\\oxjs.txt'
 
-# A map from tag name to corresponding type for SVG tags
-_SVG_TYPES = {
-    'a': 'SVGAElement',
-    'altGlyph': 'SVGElement',
-    'altGlyphDef': 'SVGElement',
-    'altGlyphItem': 'SVGElement',
-    'animate': 'SVGAnimateElement',
-    'animateColor': 'SVGElement',
-    'animateMotion': 'SVGAnimateMotionElement',
-    'animateTransform': 'SVGAnimateTransformElement',
-    'circle': 'SVGCircleElement',
-    'clipPath': 'SVGClipPathElement',
-    'color-profile': 'SVGElement',
-    'cursor': 'SVGCursorElement',
-    'defs': 'SVGDefsElement',
-    'desc': 'SVGDescElement',
-    'discard': 'SVGDiscardElement',
-    'ellipse': 'SVGEllipseElement',
-    'feBlend': 'SVGFEBlendElement',
-    'feColorMatrix': 'SVGFEColorMatrixElement',
-    'feComponentTransfer': 'SVGFEComponentTransferElement',
-    'feComposite': 'SVGFECompositeElement',
-    'feConvolveMatrix': 'SVGFEConvolveMatrixElement',
-    'feDiffuseLighting': 'SVGFEDiffuseLightingElement',
-    'feDisplacementMap': 'SVGFEDisplacementMapElement',
-    'feDistantLight': 'SVGFEDistantLightElement',
-    'feDropShadow': 'SVGFEDropShadowElement',
-    'feFlood': 'SVGFEFloodElement',
-    'feFuncA': 'SVGFEFuncAElement',
-    'feFuncB': 'SVGFEFuncBElement',
-    'feFuncG': 'SVGFEFuncGElement',
-    'feFuncR': 'SVGFEFuncRElement',
-    'feGaussianBlur': 'SVGFEGaussianBlurElement',
-    'feImage': 'SVGFEImageElement',
-    'feMerge': 'SVGFEMergeElement',
-    'feMergeNode': 'SVGFEMergeNodeElement',
-    'feMorphology': 'SVGFEMorphologyElement',
-    'feOffset': 'SVGFEOffsetElement',
-    'fePointLight': 'SVGFEPointLightElement',
-    'feSpecularLighting': 'SVGFESpecularLightingElement',
-    'feSpotLight': 'SVGFESpotLightElement',
-    'feTile': 'SVGFETileElement',
-    'feTurbulence': 'SVGFETurbulenceElement',
-    'filter': 'SVGFilterElement',
-    'font': 'SVGElement',
-    'font-face': 'SVGElement',
-    'font-face-format': 'SVGElement',
-    'font-face-name': 'SVGElement',
-    'font-face-src': 'SVGElement',
-    'font-face-uri': 'SVGElement',
-    'foreignObject': 'SVGForeignObjectElement',
-    'g': 'SVGGElement',
-    'glyph': 'SVGElement',
-    'glyphRef': 'SVGElement',
-    'hatch': 'SVGElement',
-    'hatchpath': 'SVGElement',
-    'hkern': 'SVGElement',
-    'image': 'SVGImageElement',
-    'line': 'SVGLineElement',
-    'linearGradient': 'SVGLinearGradientElement',
-    'marker': 'SVGMarkerElement',
-    'mask': 'SVGMaskElement',
-    'mesh': 'SVGElement',
-    'meshgradient': 'SVGElement',
-    'meshpatch': 'SVGElement',
-    'meshrow': 'SVGElement',
-    'metadata': 'SVGMetadataElement',
-    'missing-glyph': 'SVGElement',
-    'mpath': 'SVGMPathElement',
-    'path': 'SVGPathElement',
-    'pattern': 'SVGPatternElement',
-    'polygon': 'SVGPolygonElement',
-    'polyline': 'SVGPolylineElement',
-    'radialGradient': 'SVGRadialGradientElement',
-    'rect': 'SVGRectElement',
-    'set': 'SVGSetElement',
-    'svg': 'SVGSVGElement',
-    'solidcolor': 'SVGElement',
-    'stop': 'SVGStopElement',
-    'switch': 'SVGSwitchElement',
-    'symbol': 'SVGSymbolElement',
-    'text': 'SVGTextElement',
-    'textPath': 'SVGTextPathElement',
-    'title': 'SVGTitleElement',
-    'tref': 'SVGElement',
-    'tspan': 'SVGTSpanElement',
-    'unknown': 'SVGElement',
-    'use': 'SVGUseElement',
-    'view': 'SVGViewElement',
-    'vkern': 'SVGElement'
-}
-
-
-def generate_html_elements(ctx, n):
-    for i in range(n):
-        tag = random.choice(list(_HTML_TYPES))
-        tagtype = _HTML_TYPES[tag]
-        ctx['htmlvarctr'] += 1
-        varname = 'htmlvar%05d' % ctx['htmlvarctr']
-        ctx['htmlvars'].append({'name': varname, 'type': tagtype})
-        ctx['htmlvargen'] += '/* newvar{' + varname + ':' + tagtype + '} */ var ' + varname + ' = document.createElement(\"' + tag + '\"); //' + tagtype + '\n'
-
-
-def add_html_ids(matchobj, ctx):
-    tagname = matchobj.group(0)[1:-1]
-    if tagname in _HTML_TYPES:
-        ctx['htmlvarctr'] += 1
-        varname = 'htmlvar%05d' % ctx['htmlvarctr']
-        ctx['htmlvars'].append({'name': varname, 'type': _HTML_TYPES[tagname]})
-        ctx['htmlvargen'] += '/* newvar{' + varname + ':' + _HTML_TYPES[tagname] + '} */ var ' + varname + ' = document.getElementById(\"' + varname + '\"); //' + _HTML_TYPES[tagname] + '\n'
-        return matchobj.group(0) + 'id=\"' + varname + '\" '
-    elif tagname in _SVG_TYPES:
-        ctx['svgvarctr'] += 1
-        varname = 'svgvar%05d' % ctx['svgvarctr']
-        ctx['htmlvars'].append({'name': varname, 'type': _SVG_TYPES[tagname]})
-        ctx['htmlvargen'] += '/* newvar{' + varname + ':' + _SVG_TYPES[tagname] + '} */ var ' + varname + ' = document.getElementById(\"' + varname + '\"); //' + _SVG_TYPES[tagname] + '\n'
-        return matchobj.group(0) + 'id=\"' + varname + '\" '
-    else:
-        return matchobj.group(0)
-
-"""
-def generate_function_body(jsgrammar, htmlctx, num_lines):
-    js = ''
-    js += 'var fuzzervars = {};\n\n'
-    js += "SetVariable(fuzzervars, window, 'Window');\nSetVariable(fuzzervars, document, 'Document');\nSetVariable(fuzzervars, document.body.firstChild, 'Element');\n\n"
-    js += '//beginjs\n'
-    js += htmlctx['htmlvargen']
-    js += jsgrammar._generate_code(num_lines, htmlctx['htmlvars'])
-    js += '\n//endjs\n'
-    js += 'var fuzzervars = {};\nfreememory()\n'
-    return js
-"""
 
 def check_grammar(grammar):
     """Checks if grammar has errors and if so outputs them.
@@ -331,21 +63,38 @@ def generate_new_sample(template,jsgrammar):
     """
 
     result = template
-    
-    handlers = False
-    while '<jsfuzzer>' in result:
-        numlines = _N_MAIN_LINES
-        if handlers:
-            numlines = _N_EVENTHANDLER_LINES
-        else:
-            handlers = True
-        result = result.replace(
+    numlines = _N_MAIN_LINES
+    result = result.replace(
             '<jsfuzzer>',
             jsgrammar._generate_code(numlines),
             1
         )
 
     return result
+
+def generate_samples_internal(numbers):
+    
+    samples=[]
+
+    f = open(_INTERNAL_TEMPLATE_PATH)
+    template = f.read()
+    f.close()
+   
+    jsgrammar = Grammar()
+    err = jsgrammar.parse_from_file(_INTERNAL_SYNTAX_PATH)
+   
+    if err > 0:
+        print('There were errors parsing grammar')
+        return
+
+    for i in range(numbers):
+        result=generate_new_sample(template,jsgrammar)
+
+        if result is not None:
+            samples.append(result)
+            print('Write sample No.'+str(i))
+
+    return samples       
 
 
 def generate_samples(grammar_dir, outfiles):
